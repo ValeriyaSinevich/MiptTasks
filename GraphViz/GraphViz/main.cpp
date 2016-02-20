1a58db3fa81722cf0c7beeb4e72473189be27c60
ValeriyaSinevich <Valeriya.Sinevich@phystech.edu>
2016-02-20 20:27:55 +0300

    check

--- GraphViz/GraphViz/main.cpp
+++ GraphViz/GraphViz/main.cpp
@@ -7,19 +7,6 @@ int main(int argc, char *argv[])
 {
 	
 	QApplication a(argc, argv);
-
-	/*BSTEPA *b = new BSTEPA();
-	QMetaObject::Connection x = QObject::connect(&b->a, SIGNAL(smth(void)), b, SLOT(handler(void)));
-	if (x) {
-		qDebug() << "great!\n";
-	}
-	
-	b->a.emitter();
-	int xx = 1;
-	for (int i = 0; i < 1000 * 1000 * 10; ++i) {
-		xx += xx * xx * xx;
-	}
-	qDebug() << xx << "\n";*/
 	GraphViz w;
 	w.show();
 	return a.exec();

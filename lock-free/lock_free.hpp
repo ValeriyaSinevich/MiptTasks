cf30ad39c3dd068b8953ce48429a71454a4aae42
ValeriyaSinevich <Valeriya.Sinevich@phystech.edu>
2016-05-08 18:54:00 +0300

    hazard

--- lock-free/lock_free.hpp
+++ lock-free/lock_free.hpp
@@ -9,8 +9,115 @@
 #include <cstdio>
 #include <iostream>
 
+template <typename T>
+class memory;
+
+unsigned const max_hazard_pointers = 100;
+
+
+struct hazard_pointer
+{
+	std::atomic<std::thread::id> id;
+	std::atomic<void*> pointer;
+};
+
+hazard_pointer hazard_pointers[max_hazard_pointers];
+
+class hp_owner
+{
+	hazard_pointer* hp;
+
+public:
+	hp_owner(hp_owner const&) = delete;
+
+	hp_owner operator=(hp_owner const&) = delete;
+
+	hp_owner() : hp(nullptr) {
+		for (unsigned i = 0; i < max_hazard_pointers; ++i) {
+			std::thread::id old_id;
+			if (hazard_pointers[i].id.compare_exchange_strong(
+				old_id, std::this_thread::get_id())) {
+				hp = &hazard_pointers[i];
+				break;
+			}
+		}
+		if (!hp) {
+			throw std::runtime_error("No hazard pointers available");
+		}
+	}
+	std::atomic<void*>& get_pointer(){
+		return hp->pointer;
+	}
+	~hp_owner() {
+		hp->pointer.store(nullptr);
+		hp->id.store(std::thread::id());
+	}
+};
+
+std::atomic<void*>& get_hazard_pointer_for_current_thread()
+{
+	thread_local static hp_owner hazard;
+	return hazard.get_pointer();
+}
+
 template<typename T>
 struct node;
+
+bool outstanding_hazard_pointers_for(void* p) {
+	for (unsigned i = 0; i < max_hazard_pointers; ++i) {
+		if (hazard_pointers[i].pointer.load() == p) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+template<typename T>
+void do_delete(void* p)
+{
+	delete static_cast<T*>(p);
+}
+struct data_to_reclaim
+{
+	void* data;
+	std::function<void(void*)> deleter;
+	data_to_reclaim* next;
+	template<typename T>
+	data_to_reclaim(T* p) :
+		data(p),
+		deleter(&do_delete<T>),
+		next(0) {}
+	~data_to_reclaim() {
+		deleter(data);
+	}
+};
+
+std::atomic<data_to_reclaim*> nodes_to_reclaim;
+
+void add_to_reclaim_list(data_to_reclaim* node) {
+	node->next = nodes_to_reclaim.load();
+	while (!nodes_to_reclaim.compare_exchange_weak(node->next, node));
+}
+
+template<typename T>
+void reclaim_later(T* data) {
+	add_to_reclaim_list(new data_to_reclaim(data));
+}
+
+void delete_nodes_with_no_hazards() {
+	data_to_reclaim* current = nodes_to_reclaim.exchange(nullptr);
+	while (current) {
+		data_to_reclaim* const next = current->next;
+		if (!outstanding_hazard_pointers_for(current->data)) {
+			delete current;
+		}
+		else {
+			add_to_reclaim_list(current);
+		}
+		current = next;
+	}
+}
 
 template <typename T>
 class memory {
@@ -49,9 +156,7 @@ struct node
 {
 	std::shared_ptr<T> data;
 	node* next;
-	node(T const& data_) :
-		data(std::make_shared<T>(data_))
-	{}
+	node(T const& data_) : data(std::make_shared<T>(data_)) {}
 	node(){ data = nullptr; }
 	~node(){}
 };
@@ -69,7 +174,7 @@ public:
 		head = nullptr;
 	}
 	void push(T const & data){
-		Node* const new_node = mem.node_malloc();
+		Node* const new_node = new Node(data);
 		if (new_node == nullptr) {
 			std::cout << "no memory" << std::endl;
 		}
@@ -78,11 +183,31 @@ public:
 		while (!head.compare_exchange_weak(new_node->next, new_node));
 }
 	void pop(){
+		std::atomic<void*>& hp = get_hazard_pointer_for_current_thread();
 		Node* old_head = head.load();
-		while (old_head &&
-			!head.compare_exchange_weak(old_head, old_head->next));
-		if (old_head)
-			mem.node_free(old_head);
+		do {
+			node* temp;
+			do {
+				temp = old_head;
+				hp.store(old_head);
+				old_head = head.load();
+			} while (old_head != temp);
+		} while (old_head &&
+			!head.compare_exchange_strong(old_head, old_head->next));
+
+		hp.store(nullptr);
+		std::shared_ptr<T> res;
+		if (old_head) {
+			res.swap(old_head->data);
+			if (outstanding_hazard_pointers_for(old_head)) {
+				reclaim_later(old_head);
+			}
+			else {
+				delete old_head;
+			}
+			delete_nodes_with_no_hazards();
+		}
+		return res;
 	}
 };
 
--- lock-free/tests.cpp
+++ lock-free/tests.cpp
@@ -1,8 +1,8 @@
 #include "lock_free.hpp"
 #include <thread>
-//#define BOOST_TEST_MODULE MyTest
-//#define BOOST_TEST_MAIN
-//#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
 
 void push(lock_free_stack<int> & stack, std::atomic<int> & size) {
 	for (int i = 0; i < 100000; ++i) {
@@ -20,8 +20,10 @@ void pop(lock_free_stack<int> & stack, std::atomic<int> & size) {
 
 
 
-//BOOST_AUTO_TEST_CASE(test)
-int main(){
+
+
+BOOST_AUTO_TEST_CASE(test) {
+//int main(){
 	lock_free_stack<int> stack;
 	std::atomic<int> size = 50;
 
@@ -49,4 +51,35 @@ int main(){
 	else {
 		printf("ABA problem, size = %d", size);
 	}
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test) {
+	//int main(){
+	lock_free_stack<int> stack;
+	std::atomic<int> size = 50;
+
+	std::vector<std::thread> threads;
+	int numberOfThreads = std::thread::hardware_concurrency() / 2;
+
+	for (int i = 0; i < numberOfThreads; ++i) {
+		threads.push_back(std::thread(push, std::ref(stack), std::ref(size)));
+	}
+	for (int i = 0; i < numberOfThreads; ++i) {
+		threads.push_back(std::thread(pop, std::ref(stack), std::ref(size)));
+	}
+
+	for (std::vector<std::thread>::iterator i = threads.begin(); i < threads.end(); ++i) {
+		(*i).join();
+	}
+
+	//BOOST_CHECK_EQUAL(size, 50);
+	if (size == 50) {
+		printf("Everything ok");
+	}
+	else {
+		printf("ABA problem, size = %d", size);
+	}
+
 }
